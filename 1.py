4dd59db9808a37ca23c2f07674118e8052ab59e9
chenchuk77 <chenchuk@gmail.com>
2023-12-24 14:21:02 +0200

    init

---
+++ 1.py
@@ -0,0 +1,20 @@
+from langchain.llms import Ollama
+
+
+llm = Ollama(model="orca-mini", temperature=0)
+
+
+
+
+
+
+
+#### TODO:
+
+1. create github
+2. push repo
+3. pull from fgpt
+4. open 888 sec-group
+5. run remote notebook
+6. access remotely fgpt:8888
+7. run notebook with local ollama
